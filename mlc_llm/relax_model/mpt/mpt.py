bf0b07eb02fa20eede62f1d071aa8ea257c82da3
Valery Chernov <valery.chernov@deelvin.com>
2023-07-11 14:08:25 +0300

    rollback more

--- mlc_llm/relax_model/mpt/mpt.py
+++ mlc_llm/relax_model/mpt/mpt.py
@@ -127,7 +127,7 @@ def scaled_multihead_dot_product_attention(
         (attn_bias.struct_info.shape[-2] != 1 and
           attn_bias.struct_info.shape[-2] != s_q)): # dynamic condition?
       raise RuntimeError(f'attn_bias (shape: {attn_bias.struct_info.shape}) is expected to broadcast to shape: {attn_weight.struct_info.shape}.')
-    # attn_weight = attn_weight + attn_bias
+    attn_weight = attn_weight + attn_bias
   min_val = get_type_min_val(q)
   if key_padding_mask is not None:
     print("USE KEY MASK")
@@ -147,7 +147,7 @@ def scaled_multihead_dot_product_attention(
       causal_mask = nn.emit(relax.op.strided_slice(causal_mask, [0, 1], [s_q_end - s_q, s_k_end - s_k], [s_q_end, s_k_end]))
       causal_mask = nn.emit(relax.op.reshape(causal_mask, (1, 1, s_q, s_k)))
       attn_weight = nn.emit(relax.op.masked_fill(attn_weight, causal_mask, min_val))
-  # attn_weight = nn.emit(relax.op.nn.softmax(attn_weight))
+  attn_weight = nn.emit(relax.op.nn.softmax(attn_weight))
   out = nn.emit(relax.op.matmul(attn_weight, v))
   out = reverse_reshape_and_permute(out)
 
