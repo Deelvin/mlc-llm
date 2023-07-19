54ce33d99a23afbe7357769f45190dbec7427a01
Valery Chernov <valery.chernov@deelvin.com>
2023-07-19 15:09:16 +0300

    print relax param names

--- mlc_llm/relax_model/modules.py
+++ mlc_llm/relax_model/modules.py
@@ -232,4 +232,5 @@ def named_parameters(model: nn.Module) -> Dict[str, nn.Parameter]:
         elif isinstance(module, nn.Module):
             for param_name, param in named_parameters(module).items():
                 params[f"{name}.{param_name}"] = param
+    print("RELAX PARAMS:", params)
     return params
