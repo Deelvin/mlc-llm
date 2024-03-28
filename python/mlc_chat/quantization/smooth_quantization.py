0cf0f9fd9997e965db06517875d085677eac5816
Sergey Shtin <sergey.shtin@deelvin.com>
2024-03-28 11:22:30 +0000

    SmoothQuant parametrization improvements.

--- python/mlc_chat/quantization/smooth_quantization.py
+++ python/mlc_chat/quantization/smooth_quantization.py
@@ -10,43 +10,45 @@ from ..loader import QuantizeMapping
 
 @dataclass
 class SmoothQuantize:  # pylint: disable=too-many-instance-attributes
-  name: str
-  kind: str
-  quantize_dtype: Literal["int8"]
-  storage_dtype: Literal["int8"]
-  model_dtype: Literal["float16"]
+    name: str
+    kind: str
+    activation_dtype: Literal["int8", "e4m3_float8", "e5m2_float8"]
+    weight_dtype: Literal["int8", "e4m3_float8", "e5m2_float8"]
+    zero_point_dtype: Literal["int8", "float16", "float16"]
+    accumulator_dtype: Literal["int32", "float32", "float32"]
+    model_dtype: Literal["float16"]
 
-  def __post_init__(self):
-    return
+    def __post_init__(self):
+        return
 
-  def quantize_weight(self, weight: NDArray) -> List[NDArray]:
-     return []
+    def quantize_weight(self, weight: NDArray) -> List[NDArray]:
+        return []
 
-  def quantize_model(
+    def quantize_model(
         self,
         model: nn.Module,
         quant_map: QuantizeMapping,
         name_prefix: str,
     ) -> nn.Module:
-      """
-      Quantize model with using smooth quantization
-      currently all conversions are performed using compilation passes.
-      ToDo: apply final model conversion using this pass.
+        """
+        Quantize model with using smooth quantization
+        currently all conversions are performed using compilation passes.
+        ToDo: apply final model conversion using this pass.
 
-      Parameters
-      ----------
-      model : nn.Module
-          The non-quantized nn.Module.
+        Parameters
+        ----------
+        model : nn.Module
+            The non-quantized nn.Module.
 
-      quant_map : QuantizeMapping
-          The quantize mapping with name mapping and func mapping.
+        quant_map : QuantizeMapping
+            The quantize mapping with name mapping and func mapping.
 
-      name_prefix : str
-          The name prefix for visited weight.
+        name_prefix : str
+            The name prefix for visited weight.
 
-      Returns
-      -------
-      ret : nn.Module
-          The quantized nn.Module.
-      """
-      return model
+        Returns
+        -------
+        ret : nn.Module
+            The quantized nn.Module.
+        """
+        return model
